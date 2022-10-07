ae926c77825f79ad15b2e4d6bae386a434f49700
hyunsunny3 <47172698+hyunsunny3@users.noreply.github.com>
2022-10-07 16:15:36 +0900

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+$(function () {
+  //   $('.depth1').click(function(e){
+  //     e.preventDefault();
+  //     $('.depth2').stop().slideToggle();
+  // });
+
+  $('.menu_m').on('click',function () {
+      $('nav').animate({left:'0'},300);
+  });
+  $('.menu_m').on('click',function () {
+      $('.nav_bg').show();
+  });
+  $('.nav_bg').on('click',function () {
+      $('nav').animate({left:'-80%'},300);
+      $('.nav_bg').hide();
+  });
+  // $('.nav_open').on('click',function () {
+  //   $('nav').animate({left:'0'},300);
+  // });
+});
